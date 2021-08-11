892a73f2779dca9be7e3d8bdffc0e537c9154249
Santiago Cuervo - Ceiba Software <santiago.cuervo@ceiba.com.co>
2021-08-11 17:32:44 -0500

    estructura base

---
+++ src/test/java/Runner.java
@@ -0,0 +1,9 @@
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions()
+public class Runner {
+
+}
